d0588ff7ceac5f58a3a835978e21ad1a6504bb28
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-17 17:18:49 -0500

    transfered prototype webpage to project

---
+++ homepage/js/js-test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log("This is a JS Test");
+
+var colorButton = document.getElementById('testButton');
+
+
+
+// colorButton.onclick = function () {
+//     this.style.backgroundColor = randomColor;
+// };
+
+function change (){
+    var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+    var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
+    colorButton.style.backgroundColor = randomColor;
+    var newColor = randomColor
+    console.log(newColor);
+}
