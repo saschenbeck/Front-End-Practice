ed310be927e209b9f89428560141e44a6b3a7fe7
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-09-30 12:34:37 -0500

    feat: animated loading screen

--- homepage/js/homepageVer2.js
+++ homepage/js/homepageVer2.js
@@ -1,7 +1,35 @@
 "use strict";
 
-$(window).on("load", function () {
+$(document).ready(function() {
     setTimeout(function () {
-        $(".preloader").addClass("complete")
-    },2000)
-})
+        loadUp();
+        backgroundColor($(".content"));
+        let i = 0;
+        for (let item of $(".col-2")){
+            i += 500;
+            setTimeout(function () {
+                columnAppear(item);
+            },i)
+        }
+    },1500)
+});
+
+
+function loadUp() {
+    $(".loader").fadeOut();
+    for (let item of $(".content")) {
+        item.style.opacity = 1;
+    };
+};
+
+function backgroundColor(element) {
+    element.addClass('blue');
+}
+
+
+function columnAppear(item){
+        let rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+        item.style.backgroundColor = rainbow[Math.floor(Math.random() * rainbow.length)];
+        item.style.opacity = 1;
+}
+
